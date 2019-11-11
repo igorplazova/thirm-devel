8aa19d81101a7c93abafc06606fb9b3ecbdcd6eb
therealmoney <31512414+therealmoney@users.noreply.github.com>
2019-11-12 04:42:07 +0530

    Add files via upload

--- trade.js
+++ trade.js
@@ -16,6 +16,8 @@ $(".coin2").text(coin2);
 
 
 
+
+
 //functions
 function logout(){
 usertoken =  localStorage.removeItem("usertoken");
@@ -65,19 +67,44 @@ $.ajax({
 	   	 document.getElementById("usdtbalance").innerText = mydata.eth + " USDT";
 
 	 mmydata = mydata.orders;
+	 mmydata2 = mydata.orders_history_make;
+	 mmydata3 = mydata.orders_history_take;
 
 	 mmydata.forEach(function(elementp) {
 
 	  var table = document.getElementById("myTablemy");
-      var row = table.insertRow(-1);
+    var row = table.insertRow(-1);
 
    row.insertCell(0).innerHTML = `${elementp.id}`;
    row.insertCell(1).innerHTML = `${elementp.have2} ${elementp.have}`;
    row.insertCell(2).innerHTML = `${elementp.want2} ${elementp.want}`;
    row.insertCell(3).innerHTML = `<button  class="button is-small is-danger"  onclick=ordercancel("${elementp.id}")>Cancel</button>`;
 
+	 });
+
+	 mmydata2.forEach(function(elementp) {
+
+		var table = document.getElementById("myTablemy2");
+		 var row = table.insertRow(-1);
+
+		row.insertCell(0).innerHTML = `${elementp.id}`;
+		row.insertCell(1).innerHTML = `${elementp.have2} ${elementp.have}`;
+		row.insertCell(2).innerHTML = `${elementp.want2} ${elementp.want}`;
 
 	 });
+
+	 mmydata3.forEach(function(elementp) {
+
+		var table = document.getElementById("myTablemy3");
+		 var row = table.insertRow(-1);
+
+		row.insertCell(0).innerHTML = `${elementp.id}`;
+		row.insertCell(1).innerHTML = `${elementp.have2} ${elementp.have}`;
+		row.insertCell(2).innerHTML = `${elementp.want2} ${elementp.want}`;
+
+	 });
+
+
 
    },
    error : function(result) {
@@ -98,6 +125,8 @@ mydata.forEach(function(elementp) {
    var row = table.insertRow(-1);
    var pukki = elementp.haveamount/elementp.wantamount;
 
+	 graph(elementp.time,pukki);
+
    row.insertCell(0).innerHTML = `${elementp.time}`;
    row.insertCell(1).innerHTML = `${elementp.id}`;
    row.insertCell(2).innerHTML = `${elementp.have}`;
@@ -113,6 +142,35 @@ mydata.forEach(function(elementp) {
  });
 
 
+ $.ajax({
+    type : "GET",
+    url : "https://api.thirm.com/orderhistory?have="+ coin2 +"&want="+coin1,
+    beforeSend: function(xhr){xhr.setRequestHeader('token', usertoken);},
+    success : function(result) {
+    mydata = result;
+
+
+
+ mydata.forEach(function(elementp) {
+    var table = document.getElementById("myTable22");
+    var row = table.insertRow(-1);
+    var pukki = elementp.wantamount/elementp.haveamount;
+
+	 graph(elementp.time,pukki);
+
+    row.insertCell(0).innerHTML = `${elementp.time}`;
+    row.insertCell(1).innerHTML = `${elementp.id}`;
+    row.insertCell(2).innerHTML = `${elementp.have}`;
+    row.insertCell(3).innerHTML = `${elementp.want}`;
+    row.insertCell(4).innerHTML = `${elementp.haveamount}`;
+    row.insertCell(5).innerHTML = `${pukki}`;
+    row.insertCell(6).innerHTML = `${elementp.wantamount}`;
+ });
+    },
+    error : function(result) {
+     location.reload(true);
+    }
+  });
 
 
 function ordercancel(inputnow){
