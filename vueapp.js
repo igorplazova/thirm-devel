ec65e234c280ce4e4f7e07ec3cad37d91b68a4ed
igorplazova <igorplazova@yandex.com>
2019-12-23 21:54:10 +0330

    trade_history

---
+++ vueapp.js
@@ -0,0 +1,28 @@
+
+
+new Vue({
+    el: '#trade-app',
+    data () {
+        var data = {};
+        data.coins = [{symbol:'btc'},{symbol:'ltc'},{symbol:'usdt'},{symbol:'eth'}];
+        data.altcoins = data.coins.filter((function(c){return c.symbol!='btc'}));
+        data.coin1 = getSearchParams("coin1") || 'btc';
+        data.coin2 = getSearchParams("coin2") || 'eth';
+        data.trade_history=[];
+        return data;
+    },
+    mounted () {
+        axios
+          .get("https://api1.thirm.com/public/trades/"+ this.coin1 +"_" + this.coin2)
+          .then(response => (this.trade_history = response.data))
+      }
+  });
+
+
+  
+
+function getSearchParams(k){
+    var p={};
+    location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(s,k,v){p[k]=v})
+    return k?p[k]:p;
+   }
